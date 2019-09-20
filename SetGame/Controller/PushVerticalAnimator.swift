40c218314ea3c34e7e5f5f2697846d8472ab6a37
lukaslizal <luky.lizal@gmail.com>
2019-09-20 10:31:23 +0200

    Custom transition fix.

--- SetGame/Controller/PushVerticalAnimator.swift
+++ SetGame/Controller/PushVerticalAnimator.swift
@@ -23,7 +23,7 @@ public class PushVerticalAnimator: NSObject, UIViewControllerAnimatedTransitioni
         guard let from = transitionContext.view(forKey: UITransitionContextViewKey.from) else { return }
         guard let to = transitionContext.view(forKey: UITransitionContextViewKey.to) else { return }
         container.addSubview(to)
-        to.frame = presenting ? from.frame.offsetBy(dx: 0, dy: -to.layer.bounds.height) : from.frame.offsetBy(dx: 0, dy: to.layer.bounds.height)
+        to.frame = presenting ? from.frame.offsetBy(dx: 0, dy: -from.layer.bounds.height) : from.frame.offsetBy(dx: 0, dy: from.layer.bounds.height)
         UIView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 1, options: [], animations: {
             to.frame = self.presenting ? to.frame.offsetBy(dx: 0, dy: to.layer.bounds.height) : to.frame.offsetBy(dx: 0, dy: -to.layer.bounds.height)
             from.frame = self.presenting ? from.frame.offsetBy(dx: 0, dy: from.layer.bounds.height) : from.frame.offsetBy(dx: 0, dy: -from.layer.bounds.height)
